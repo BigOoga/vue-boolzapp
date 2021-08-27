ec1a9efd801f1ed14327f10664446e989190907f
BigOoga <edoardo.ruffi@gmail.com>
2021-08-27 14:29:45 +0200

    Added timestamp support, timestamp function  and reply emulation

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const root = new Vue({
     contacts: data.contacts,
     searchKey: "",
     currentIndex: 0,
+    messageBoxContent: "",
   },
   methods: {
     isActive(index) {
@@ -16,6 +17,41 @@ const root = new Vue({
     },
     setNewIndex(index) {
       this.currentIndex = index;
+    },
+    sendMsg() {
+      let currentMsg = {
+        date: this.getTimestamp(),
+        message: this.messageBoxContent,
+        status: "sent",
+      };
+
+      this.contacts[this.currentIndex].messages.push(currentMsg);
+      this.messageBoxContent = "";
+
+      this.emulateReply(this.currentIndex);
+    },
+    getTimestamp() {
+      const timestamp = dayjs().format("DD/MM/YYYY HH:mm:ss");
+      return timestamp;
+    },
+
+    receiveMsg(message, index) {
+      // Index è l'indice del contatto da cui si riceve il messaggio
+
+      currentMsg = message;
+      currentMsg.date = this.getTimestamp();
+      this.contacts[index].messages.push(currentMsg);
+    },
+
+    emulateReply() {
+      // Simula una risposta con 2 secondi di ritardo
+      let currentMsg = {
+        date: "",
+        message: "Beep boop",
+        status: "received",
+      };
+
+      setTimeout(this.receiveMsg, 2000, currentMsg, this.currentIndex);
     },
   },
 });
